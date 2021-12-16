c755fe8f30e5518c9dd1b29286bd9006ebf7cb80
Bronco <heybronco@gmail.com>
2021-12-16 16:57:04 +0800

    Create basic.js

---
+++ basic.js
@@ -0,0 +1,11 @@
+let obj = JSON.parse($response.body || '{}');
+
+if Array.isArray(obj.Variables.data) {
+  obj.Variables.data.forEach((item) => {
+    item.adv = {};
+  });
+}
+
+$done({
+	body: JSON.stringify(obj)
+});
