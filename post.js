8d88d0943b1596e22daabb85972347a727443a99
Bronco <heybronco@gmail.com>
2021-12-16 17:09:15 +0800

    Update post.js

--- post.js
+++ post.js
@@ -1,27 +1,12 @@
 let obj = JSON.parse($response.body || '{}');
 
-if (obj.tagadv) {
-	obj.tagadv = "";
-}
-
-if (obj.threadapp_ad_video) {
-	obj.threadapp_ad_video = [];
-}
-
-if (obj.pingyouadv) {
-	obj.pingyouadv = "";
-}
-
-if (obj.middleadv) {
-	obj.middleadv = "";
-}
-
-if (obj.bottomadv) {
-	obj.bottomadv = "";
-}
-
-if (obj.appdetailadv) {
-	obj.appdetailadv = [];
+if (obj.Variables.data.threaddetail) {
+	obj.Variables.data.threaddetail.tagadv = "";
+	obj.Variables.data.threaddetail.threadapp_ad_video = [];
+	obj.Variables.data.threaddetail.pingyouadv = "";
+	obj.Variables.data.threaddetail.middleadv = "";
+	obj.Variables.data.threaddetail.bottomadv = "";
+	obj.Variables.data.threaddetail.appdetailadv = [];
 }
 
 $done({
