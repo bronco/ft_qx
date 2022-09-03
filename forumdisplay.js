2e5902982cc53da828939ce4282ed355b3e20183
Bronco <heybronco@gmail.com>
2022-09-03 12:12:57 +0800

    bugfix

--- forumdisplay.js
+++ forumdisplay.js
@@ -1,6 +1,6 @@
 let obj = JSON.parse($response.body || '{}');
 
-if Array.isArray(obj.Variables.data.forum_threadlist) {
+if (Array.isArray(obj.Variables.data.forum_threadlist)) {
   const newlist = obj.Variables.data.forum_threadlist.filter(item => item.author != '官方');
   obj.Variables.data.forum_threadlist = newlist;
   obj.Variables.data.tpp = newlist.length;
--- forumlist.js
+++ forumlist.js
@@ -1,6 +1,6 @@
 let obj = JSON.parse($response.body || '{}');
 
-if Array.isArray(obj.Variables.data) {
+if (Array.isArray(obj.Variables.data)) {
   obj.Variables.data.forEach((item) => {
     item.adv = {};
   });
