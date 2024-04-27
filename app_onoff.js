e8ba22d47ec5831c59c79bec9e13f395efc202e4
John Lee <li.xiang1@coscoshipping.com>
2024-04-27 19:18:36 +0800

    Add app_onoff

---
+++ app_onoff.js
@@ -0,0 +1,11 @@
+let obj = JSON.parse($response.body || '{}');
+
+if (obj.Variables.data.topthreads) {
+  obj.Variables.notice.newprompt = 0;
+  obj.Variables.notice.data.diamondlist = [];
+  obj.Variables.notice.data.newtodolist  = obj.Variables.notice.data.newtodolist.filter(item => item.title == '飞米兑换');
+}
+
+$done({
+	body: JSON.stringify(obj)
+});
