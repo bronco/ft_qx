19f3f6bd3c82256b768e236d7c84ed3ab2e979a3
John Lee <li.xiang1@coscoshipping.com>
2024-04-27 19:32:27 +0800

    bug fix

--- app_onoff.js
+++ app_onoff.js
@@ -1,10 +1,10 @@
 let obj = JSON.parse($response.body || '{}');
 
-if (obj.Variables.notice) {
-  obj.Variables.notice.newprompt = 0;
-  obj.Variables.notice.data.diamondlist = [];
-  obj.Variables.notice.data.newtodolist  = obj.Variables.notice.data.newtodolist.filter(item => item.title == '飞米兑换');
-}
+
+obj.Variables.notice.newprompt = "0";
+obj.Variables.data.diamondlist = [];
+obj.Variables.data.newtodolist  = obj.Variables.data.newtodolist.filter(item => item.title == '飞米兑换');
+
 
 $done({
 	body: JSON.stringify(obj)
