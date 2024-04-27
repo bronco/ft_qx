5e38cf7f6298a29508e72a845e4749f115e8ffdc
John Lee <li.xiang1@coscoshipping.com>
2024-04-27 19:43:00 +0800

    Add global filter for notice badget

--- app_onoff.js
+++ app_onoff.js
@@ -1,7 +1,5 @@
 let obj = JSON.parse($response.body || '{}');
 
-
-obj.Variables.notice.newprompt = "0";
 obj.Variables.data.diamondlist = [];
 obj.Variables.data.newtodolist  = obj.Variables.data.newtodolist.filter(item => item.title == '飞米兑换');
 
---
+++ notice_num.js
@@ -0,0 +1,9 @@
+let obj = JSON.parse($response.body || '{}');
+
+if (obj.Variables.notice) {
+    obj.Variables.notice.newprompt = "0";
+}
+
+$done({
+	body: JSON.stringify(obj)
+});
