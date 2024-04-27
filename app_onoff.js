68f819ccc35f3b32aeaf429bd9d485ac9cea38cb
John Lee <li.xiang1@coscoshipping.com>
2024-04-27 19:27:58 +0800

    Bug fix for app_onoff

--- app_onoff.js
+++ app_onoff.js
@@ -1,6 +1,6 @@
 let obj = JSON.parse($response.body || '{}');
 
-if (obj.Variables.data.topthreads) {
+if (obj.Variables.notice) {
   obj.Variables.notice.newprompt = 0;
   obj.Variables.notice.data.diamondlist = [];
   obj.Variables.notice.data.newtodolist  = obj.Variables.notice.data.newtodolist.filter(item => item.title == '飞米兑换');
